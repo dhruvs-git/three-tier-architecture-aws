bd3474ebc0dc4852d08c8675110aa6a9c8a13637
Dhruv Barot <dbarot3553@gmail.com>
2026-05-19 21:09:07 -0400

    feat: add S3 remote backend with state locking

--- versions.tf
+++ versions.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = "~> 1.14.6"
 
+  backend "s3" {
+    bucket       = "three-tier-tfstate-563955855951"
+    key          = "terraform.tfstate"
+    region       = "ca-central-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
